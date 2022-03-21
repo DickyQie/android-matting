8ae96ac9153114fc61da16b45b284e60e864e419
zhangqie <zhangqiem@126.com>
2022-03-21 11:43:05 +0800

    Modnet 算法实现人像抠图和更换背景

---
+++ app/src/main/java/com/zhangqie/matting/UtilMap.java
@@ -0,0 +1,24 @@
+package com.zhangqie.matting;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/**
+ * Created by zhangqie on 2022/3/11
+ * Describe:
+ */
+public class UtilMap {
+
+    public static Bitmap drawBg4Bitmap(int color, Bitmap orginBitmap) {
+        Paint paint = new Paint();
+        paint.setColor(color);
+        Bitmap bitmap = Bitmap.createBitmap(orginBitmap.getWidth(),
+                orginBitmap.getHeight(), orginBitmap.getConfig());
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawRect(0, 0, orginBitmap.getWidth(), orginBitmap.getHeight(), paint);
+        canvas.drawBitmap(orginBitmap, 0, 0, paint);
+        return bitmap;
+    }
+
+}
